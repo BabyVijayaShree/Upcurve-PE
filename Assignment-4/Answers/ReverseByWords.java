7252c789ee307c1311f2cd55667597e4a08cfe9c
BabyVijayaShree <112560474+BabyVijayaShree@users.noreply.github.com>
2024-03-24 14:21:35 +0530

    Update ReverseByWords.java

--- Assignment-4/Answers/ReverseByWords.java
+++ Assignment-4/Answers/ReverseByWords.java
@@ -1,1 +1,13 @@
+import java.util.*;
 
+public class ReverseByWords {
+    public static void main(String args[]) {
+      Scanner scan = new Scanner(System.in);  
+      String s[] = scan.nextLine().split(" "); 
+      String ans = ""; 
+      for (int i = s.length - 1; i >= 0; i--) { 
+        ans += s[i] + " "; 
+      } 
+      System.out.println("Reversed String: " + ans); 
+    }
+}
